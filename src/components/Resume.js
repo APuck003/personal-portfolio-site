e58a6e94e44c449dc7ea899df9664ad2f2c42a2e
Austin Puckett <puckett_austin@yahoo.com>
2019-07-02 03:18:31 -0500

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
+import Education from './Education';
+import Experience from './Experience;
+import Skills from './Skills';
 
 class Resume extends Component {
   
