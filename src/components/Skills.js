a3592e180532d02b9196f3bd72b2ab933020d821
Austin Puckett <puckett_austin@yahoo.com>
2019-07-02 03:26:33 -0500

    Created Skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Grid, Cell, ProgressBar } from 'react-mdl';
+
+class Skills extends Component {
+  render() {
+    return(
+      <Grid>
+        <Cell col={12}>
+          
+        </Cell>
+      </Grid>
+    )
+  }
+}
+
+export default Skills;
