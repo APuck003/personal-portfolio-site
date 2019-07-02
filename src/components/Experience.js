d46d888b232ad026077f8cd65cba76964f251ca4
Austin Puckett <puckett_austin@yahoo.com>
2019-07-02 03:25:04 -0500

    Created Experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Experience extends Component {
+  render() {
+    return(
+      <Grid>
+        <Cell col={4}>
+          
+        </Cell>
+      </Grid>
+    )
+  }
+}
+
+export default Experience;
